a840e9b3ef7bb0c13db212483d96221f457c3577
LucasHom <lucasrylanhom@gmail.com>
2023-10-01 12:50:21 -0700

    Search for Ladybugs not Pigeons. Small Adjustments to test surge deployment

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
       
       {isLoading ? 
-      <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :  
+      <h1 className="text-6xl text-center mx-auto mt-32">Really cool Images Loading...</h1> :  
         <div className="grid grid-cols-3 gap-4">
           {images.map(image => (
             <ImageCard key={image.id} image={image}/>
--- src/ImageSearch.js
+++ src/ImageSearch.js
@@ -11,7 +11,7 @@ const ImageSearch = ({searchText}) => {
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
             <form onSubmit={onsubmit} className="w-full max-w-sm">
                 <div className="flex items-center border-b border-b-2 border-gray-400 py-2">
-                    <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for Pigeons"/>
+                    <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for Ladybugs"/>
                     <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">Search</button>
                 </div>
             </form>
